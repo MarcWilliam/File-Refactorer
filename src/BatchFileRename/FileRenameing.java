73e2fdd35d75c16462e6a102e0a175e2af778943
Marc Wafik <marc.wafik@hotmail.com>
2016-07-30 20:27:16 +0200

    Added form GUI
    added form to  take input (path , old name ..etc )
    added table to display output

--- src/BatchFileRename/FileRenameing.java
+++ src/BatchFileRename/FileRenameing.java
@@ -1,84 +1,53 @@
 package BatchFileRename;
 
 import java.io.*;
-import java.util.*;
-import java.util.regex.*;
 
 public class FileRenameing {
 
-	public static void main(String[] args) {
-		Scanner Scan = new Scanner(System.in);
-		System.out.print("        Path : ");
-		String Path = Scan.nextLine();
-		System.out.print("      Target : ");
-		String Target = Scan.nextLine();
-		System.out.print(" Replacement : ");
-		String Replacement = Scan.nextLine();
-		System.out.print("    is regex : ");
-		String temp = Scan.nextLine();
-		if (!isYes(temp)) {
-			Target = Pattern.quote(Target);
-		}
-		System.out.print("   All Files : ");
-		temp = Scan.nextLine();
-		Boolean isFile = false;
-		if (isYes(temp)) {
-			isFile = true;
-		}
-		System.out.print(" All Folders : ");
-		temp = Scan.nextLine();
-		Boolean isDir = false;
-		if (isYes(temp)) {
-			isDir = true;
-		}
-		Test(Path, Target, Replacement, isFile, isDir);
-		System.out.print("Save Changes : ");
-		temp = Scan.nextLine();
-		if (isYes(temp)) {
-			Replace(Path, Target, Replacement, isFile, isDir);
-		}
-	}
-
-	public static void Replace(String PathLocation, String Target, String Replacement, Boolean isFile, Boolean isDir) {
-		String leftAlignFormat = "| %-3d | %-55s | %-55s |%n";
-		System.out.format("+-----+---------------------------------------------------------+---------------------------------------------------------+%n");
-		System.out.format("| No  | Original Name                                           | New Name                                                |%n");
-		System.out.format("+-----+---------------------------------------------------------+---------------------------------------------------------+%n");
-
+	public static String[][] Replace(String PathLocation, String Target, String Replacement, Boolean isFile, Boolean isDir) {
 		File folder = new File(PathLocation);
 		File[] listOfFiles = folder.listFiles();
+		String cells[][] = new String[listOfFiles.length][3];
+
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile() && isFile) {
 				String NewName = listOfFiles[i].getName().replaceAll(Target, Replacement);
 				renameFile(listOfFiles[i], PathLocation + "\\" + NewName);
-				System.out.format(leftAlignFormat, i, "Fil - " + listOfFiles[i].getName(), NewName);
+				cells[i][0] = String.valueOf(i);
+				cells[i][1] = "File || " + listOfFiles[i].getName();
+				cells[i][2] = NewName;
+
 			} else if (listOfFiles[i].isDirectory() && isDir) {
 				String NewName = listOfFiles[i].getName().replaceAll(Target, Replacement);
 				renameFile(listOfFiles[i], PathLocation + "\\" + NewName);
-				System.out.format(leftAlignFormat, i, "Dir - " + listOfFiles[i].getName(), NewName);
+				cells[i][0] = String.valueOf(i);
+				cells[i][1] = "Dire || " + listOfFiles[i].getName();
+				cells[i][2] = NewName;
 			}
 		}
-		System.out.format("+-----+---------------------------------------------------------+---------------------------------------------------------+%n");
+		return cells;
 	}
 
-	public static void Test(String PathLocation, String Target, String Replacement, Boolean isFile, Boolean isDir) {
-		String leftAlignFormat = "| %-3d | %-55s | %-55s |%n";
-		System.out.format("+-----+---------------------------------------------------------+---------------------------------------------------------+%n");
-		System.out.format("| No  | Original Name                                           | New Name                                                |%n");
-		System.out.format("+-----+---------------------------------------------------------+---------------------------------------------------------+%n");
+	public static String[][] Test(String PathLocation, String Target, String Replacement, Boolean isFile, Boolean isDir) {
 
 		File folder = new File(PathLocation);
 		File[] listOfFiles = folder.listFiles();
+		String cells[][] = new String[listOfFiles.length][3];
+
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile() && isFile) {
 				String NewName = listOfFiles[i].getName().replaceAll(Target, Replacement);
-				System.out.format(leftAlignFormat, i, "Fil - " + listOfFiles[i].getName(), NewName);
+				cells[i][0] = String.valueOf(i);
+				cells[i][1] = "File || " + listOfFiles[i].getName();
+				cells[i][2] = NewName;
 			} else if (listOfFiles[i].isDirectory() && isDir) {
 				String NewName = listOfFiles[i].getName().replaceAll(Target, Replacement);
-				System.out.format(leftAlignFormat, i, "Dir - " + listOfFiles[i].getName(), NewName);
+				cells[i][0] = String.valueOf(i);
+				cells[i][1] = "Dire || " + listOfFiles[i].getName();
+				cells[i][2] = NewName;
 			}
 		}
-		System.out.format("+-----+---------------------------------------------------------+---------------------------------------------------------+%n");
+		return cells;
 	}
 
 	public static boolean renameFile(File OldFile, String Path) {
@@ -87,9 +56,5 @@ public class FileRenameing {
 			return false;
 		}
 		return OldFile.renameTo(NewFile);
-	}
-
-	public static boolean isYes(String temp) {
-		return "Yes".equals(temp) || "YES".equals(temp) || "YEs".equals(temp) || "YeS".equals(temp) || "Y".equals(temp) || "y".equals(temp);
 	}
 }
